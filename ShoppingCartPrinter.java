5a9112edbcaa8fbe18292eef31e63ff4ded39101
unknown <melissa.rodriguez.murguia@gmail.com>
2020-02-26 00:06:31 -0600

    created instance and prompted user for information of item 1 in ShoppingCartPrinter

--- ShoppingCartPrinter.java
+++ ShoppingCartPrinter.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ShoppingCartPrinter {
     public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        String productName = "";
+        int productPrice = 0;
+        int productQuantity = 0;
+        // Create instances of ItemToPurchase for each item
+        ItemToPurchase item1 = new ItemToPurchase();
+        ItemToPurchase item2 = new ItemToPurchase();
 
+        // Prompt user for name, price, and quantity for item 1
+        System.out.println("Item 1");
+        System.out.print("Enter the item name: ");
+        productName = scnr.nextLine();
+        item1.setName(productName);
+        System.out.print("Enter the item price: ");
+        productPrice = scnr.nextInt();
+        item1.setPrice(productPrice);
+        System.out.print("Enter the item quantity: ");
+        productQuantity = scnr.nextInt();
+        item1.setQuantity(productQuantity);
     }
 }
